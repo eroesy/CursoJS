1f3bd3cae212da3092bfe28ca334e4cfe0e30183
Matheus <131892555+eroesy@users.noreply.github.com>
2023-08-13 13:43:41 -0300

    loop

---
+++ aula13/ambiente.js
@@ -0,0 +1,6 @@
+var c = 1
+while(c < 2){
+    c ++
+    console.log('tudo bem?')
+    console.log('Eae')
+}
---
+++ aula13/node_7bc271fbce6ca.tmp.js
@@ -0,0 +1,5 @@
+var c = 0
+while(c < 100000000){
+    c ++
+    console.log('tudo bem?')
+}
